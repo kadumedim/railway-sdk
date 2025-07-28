570559bc08c4515351356c7d3156dabf92c55d28
kadumedim <kadumedim123@gmail.com>
2025-07-28 19:05:59 -0300

    generated type

--- src/test.ts
+++ src/test.ts
@@ -6,15 +6,9 @@ async function main() {
       accessToken: process.env.RAILWAY_PERSONAL_TOKEN,
       /*  accessToken: Bun.env.RAILWAY_TEAM_TOKEN, */
     });
-    /* const me = await sdk.getMe();
+    const projects = await sdk.getProjects();
 
-    console.log("me:", me); */
-
-    const services = await sdk.getServices(
-      "0b749b35-37ab-4976-8139-97e18071b6cc",
-    );
-
-    console.log("services:", services);
+    console.log("projects:", projects);
   } catch (err) {
     console.error("Error fetching:", err);
   }
